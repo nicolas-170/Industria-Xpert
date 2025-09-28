64b6c3b00c1c5812c9a906d5ab7401d321d2fb36
Admin <nicolas.pc.170@gmail.com>
2025-09-27 19:39:08 -0500

    refactor: se elimina codigo para enviar correo

--- internal/gmail-apartado/domain/model/entity_gmail_apartado.go
+++ internal/gmail-apartado/domain/model/entity_gmail_apartado.go
@@ -2,7 +2,6 @@ package model
 
 type EmailApartado struct {
 	EmailTo     string `json:"emailTo,omitempty"`
-	Codigo      string `json:"codigo ,omitempty"`
 	Mensaje     string `json:"mensaje,omitempty"`
 	TipoMensaje string `json:"tipoMensaje,omitempty"`
 	Imagen      Imagen `json:"imagen,omitempty"`
